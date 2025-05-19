515b5aab074e351cfc2b437ff235dfa246793b34
D.Penguin <jose.daniel.1265@gmail.com>
2025-05-18 20:39:10 -0500

    Update

--- Actividad POO/Assets/Scripts/Characters/Playable/Abilitysystem.cs
+++ Actividad POO/Assets/Scripts/Characters/Playable/Abilitysystem.cs
@@ -5,14 +5,4 @@ using UnityEngine;
 public class Abilitysystem : MonoBehaviour
 {
     public List<Ability> abilities = new List<Ability>();
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
